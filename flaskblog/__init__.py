865fb67aa7d6649a3741c56544ceb1d68a4d8497
navassherif98 <navassherif189@gmail.com>
2021-05-29 10:49:32 +0530

    .

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='Templates')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY']='3362cbc2fad4d632c46c2ff8575cbb7c'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
