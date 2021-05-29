51a430ccc33961405a954db16e09d46ae818e800
navassherif98 <navassherif189@gmail.com>
2021-05-29 07:36:06 +0530

    .

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -16,9 +16,8 @@ login_manager.login_message_category='info'
 app.config['MAIL_SERVER']='smtp.googlemail.com'
 app.config['MAIL_PORT']=587
 app.config['MAIL_USE_TLS']=True
-# app.config['MAIL_USERNAME']=os.environ.get('EMAIL_USER')
-app.config['MAIL_USERNAME']='navassherif981@gmail.com'
-app.config['MAIL_PASSWORD']='Aneesha@98'
+app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWORD')
 mail=Mail(app)
 
 
